70f6f298798dac6588550fe6d9174f62d80ecaa8
AJMikalauskas <97716972+AJMikalauskas@users.noreply.github.com>
2022-08-27 10:19:20 -0500

    Added surge to allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500', 'http://localhost:3000', 'http://localhost'];
+const allowedOrigins = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500', 'http://localhost:3000', 'http://localhost', 'https://crypto-app-public-demo.surge.sh' ];
 
 module.exports = allowedOrigins;
