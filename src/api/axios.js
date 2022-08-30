98e51ae0c24758ab25ed93cbc37c29f6dcfffd38
AJMikalauskas <97716972+AJMikalauskas@users.noreply.github.com>
2022-08-30 11:47:28 -0500

    Testing CORS issues while deploying site to netlify

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,9 +4,9 @@ const BASE_URL = 'https://crypto-app-demo-test.herokuapp.com/' ;
 
 export default axios.create({
     baseURL: BASE_URL,
-    headers: {
-        'Access-Control-Allow-Origin' : '*'
-    }
+    // headers: {
+    //     'Access-Control-Allow-Origin' : '*'
+    // }
 })
 
 // axios private to have interceptors, to work with JWT to refresh a token if our current accessToken or refreshToken is expired.
@@ -15,7 +15,7 @@ export const axiosPrivate = axios.create({
     baseURL: BASE_URL,
     headers: { 
         'Content-Type': 'application/json', 
-        'Access-Control-Allow-Origin' : '*'
+       // 'Access-Control-Allow-Origin' : '*'
     },
     withCredentials: true
 })
