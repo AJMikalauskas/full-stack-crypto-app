8c1cec634b005585b2bb68a90f1b1969c5f7ad53
AJMikalauskas <97716972+AJMikalauskas@users.noreply.github.com>
2022-08-30 10:54:03 -0500

    Fixed API URL to heroku

--- Crypto-App-Frontend/frontend/src/api/axios.js
+++ Crypto-App-Frontend/frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-//const BASE_URL = 'https://crypto-app-demo-test.herokuapp.com/';
-const BASE_URL= "http://localhost:3500"
+//const BASE_URL = 'http://localhost:3500' --> 'https://crypto-app-demo-test.herokuapp.com/';
+const BASE_URL = 'https://crypto-app-demo-test.herokuapp.com/' ;
 
 export default axios.create({
     baseURL: BASE_URL,
